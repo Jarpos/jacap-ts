a37b8c8e9e6310dc3221bc2b67814657381f25c9
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-02-28 18:56:10 +0100

    fix: fix Getter and Setter out of bounds with negative number

--- src/library/board.ts
+++ src/library/board.ts
@@ -1,5 +1,7 @@
-export class Board<T> {
-    private Board: T[][];
+import { Cell } from "./types";
+
+export class Board {
+    private Board: Cell[][];
     public readonly Width: number;
     public readonly Height: number;
 
@@ -11,11 +13,11 @@ export class Board<T> {
         );
     }
 
-    public Get(x: number, y: number): T {
-        return this.Board[y % this.Height][x % this.Width];
+    public Get(x: number, y: number): Cell {
+        return this.Board[(y + this.Height) % this.Height][(x + this.Height) % this.Width];
     }
 
-    public Set(x: number, y: number, value: T) {
-        this.Board[y % this.Height][x % this.Width] = value;
+    public Set(x: number, y: number, value: Cell) {
+        this.Board[(y + this.Height) % this.Height][(x + this.Height) % this.Width] = value;
     }
 }
