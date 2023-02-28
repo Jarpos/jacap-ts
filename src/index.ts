973861335e3ae026db87d48ea795a8ced563eb4d
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-02-28 18:59:04 +0100

    add settings and other basic stuff to display

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-import { Board } from "./library/board";
+import { CellTest } from "./examples/randomblink";
 import { Simulator } from "./library/simulator";
 
-let canvas = document.getElementById("canvas") as HTMLCanvasElement;
-let context = canvas.getContext("2d", { alpha: false, });
+const RESOLUTION = 25;
+const WIDTH = 750;
+const HEIGHT = 750;
 
-var b = new Simulator(10, 15, );
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+const context = canvas.getContext("2d", { alpha: false, });
+
+var sim = new Simulator(WIDTH / RESOLUTION, HEIGHT / RESOLUTION)
+    .Initialize(CellTest.InitFunction);
+
+function Loop() {
+    setTimeout(() => {
+        sim.Render(context, RESOLUTION);
+        sim.AdvanceGeneration();
+        Loop();
+    }, 500);
+}
+sim.Render(context, RESOLUTION);
+Loop();
