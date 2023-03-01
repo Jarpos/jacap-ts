20fad726526911cddc95fd09c718b0730c9fb83b
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-03-01 01:43:38 +0100

    fix: fix implementation of CellTest

--- src/examples/randomblink.ts
+++ src/examples/randomblink.ts
@@ -1,12 +1,12 @@
 import { Neighborhood } from "../library/neighborhood";
 import { color, Cell } from "../library/types";
 
-export class CellTest implements Cell {
-    private I: number = 0;
+export class CellTest implements Cell<number> {
+    public State: number = 0;
 
-    constructor(i: number) { this.I = i; }
-    get Color(): color { return this.I % 2 ? "#0f0f0f" : "#717171" };
-    public Update(neighbors: Neighborhood) { this.I++; }
+    constructor(i: number) { this.State = i; }
+    get Color(): color { return this.State % 2 ? "#0f0f0f" : "#717171" };
+    public Update(neighbors: Neighborhood<number>) { this.State++; }
 
     static InitFunction() {
         return [new CellTest(0), new CellTest(1)][Math.floor(Math.random() * 2)]
