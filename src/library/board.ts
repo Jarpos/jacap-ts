b70043adf9104fcc9950caf596ab47eba73c91b2
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-03-05 19:39:45 +0100

    ref: rename Type variable; add default value for ctor

--- src/library/board.ts
+++ src/library/board.ts
@@ -1,13 +1,13 @@
-export class Board<T> {
-    private Board: T[][];
+export class Board<CellType> {
+    private Board: CellType[][];
     public readonly Width: number;
     public readonly Height: number;
 
-    constructor(width: number, height: number) {
+    constructor(width: number, height: number, defaultValue: CellType = null) {
         this.Width = width
         this.Height = height;
         this.Board = Array.from(
-            Array(height), () => new Array(width).fill(null)
+            Array(height), () => new Array(width).fill(defaultValue)
         );
     }
 
@@ -15,7 +15,7 @@ export class Board<T> {
         return this.Board[(y + this.Height) % this.Height][(x + this.Height) % this.Width];
     }
 
-    public Set(x: number, y: number, value: T) {
+    public Set(x: number, y: number, value: CellType) {
         this.Board[(y + this.Height) % this.Height][(x + this.Height) % this.Width] = value;
     }
 }
