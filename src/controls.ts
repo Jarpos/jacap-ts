ade010cd7af00f7e1701b00096b74067fd40124b
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-08-06 01:15:29 +0200

    feat: add input field for timeout length

--- src/controls.ts
+++ src/controls.ts
@@ -1,11 +1,15 @@
-const PauseButton = document.getElementById("StartStopButton");
-const StepButton = document.getElementById("SingleStepButton");
-const Canvas = document.getElementById("canvas");
+import { Utility } from "./helpers/utility";
+
+const PauseButton = document.getElementById("StartStopButton") as HTMLButtonElement;
+const StepButton = document.getElementById("SingleStepButton") as HTMLButtonElement;
+const TimeoutInput = document.getElementById("TimeoutInput") as HTMLInputElement;
+const Canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
 export var Running = false;
 
 export interface ControlSettings {
     Resolution: number;
+    Timeout: number;
     SingleStep: () => void;
     OnClickCell: (x: number, y: number) => void;
 }
@@ -15,10 +19,18 @@ export function initControls(settings: ControlSettings) {
 
     StepButton.addEventListener("click", settings.SingleStep);
 
+    TimeoutInput.value = settings.Timeout.toString();
+    TimeoutInput.addEventListener("change", () => {
+        settings.Timeout = Utility.minMax(0, Number(TimeoutInput.value), 1000);
+        TimeoutInput.value = settings.Timeout.toString();
+    });
+
     Canvas.addEventListener("click", (ev) => {
         settings.OnClickCell(
             Math.floor(ev.offsetX / settings.Resolution),
             Math.floor(ev.offsetY / settings.Resolution)
         );
     });
+
+    return settings;
 }
