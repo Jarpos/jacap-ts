070f3d16665cc2129ee78736e9eb8a55d603239e
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-08-06 12:14:38 +0200

    update to use new onclickfunction

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import { GameOfLife } from "./examples/GameOfLife";
 import { RandomBlinkTest } from "./examples/RandomBlink";
 import { Utility } from "./helpers/utility";
 import { AutomatonCanvas } from "./library/canvas";
-import { Automaton } from "./library/automaton";
+import { AutomatonDefinition } from "./library/types";
+
 
 const Automatons = [
     BriansBrain.AutomatonDefinition,
@@ -14,20 +15,15 @@ const Automatons = [
     RandomBlinkTest.AutomatonDefinition,
 ];
 
+let chosen = Automatons[3] as AutomatonDefinition<any, any>;
 const canvas = new AutomatonCanvas("canvas", 800, 800, 10);
-const automaton = Utility.createSimulation(canvas, BriansBrain.AutomatonDefinition)
+const automaton = Utility.createSimulation(canvas, chosen)
 const settings = initControls({
     Resolution: canvas.Resolution,
     Timeout: 50,
     SingleStep: () => automaton.AdvanceGeneration(),
     OnClickCell: (x: number, y: number) =>
-        automaton.SetCell(
-            x, y,
-            Utility.getNext(
-                automaton.GetCell(x, y),
-                [BriansBrain.States.On, BriansBrain.States.Dying, BriansBrain.States.Off],
-            )
-        ),
+        automaton.SetCell(x, y, chosen.OnClickFunction(automaton.GetCell(x, y))),
 });
 
 function Loop() {
