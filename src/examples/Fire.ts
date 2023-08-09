d724bb7bc6144def4576d93e1c5a39fe681f414d
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-08-09 21:20:17 +0200

    ref: simplify code a bit; fix catchesFire likelihood

--- src/examples/Fire.ts
+++ src/examples/Fire.ts
@@ -3,6 +3,9 @@ import { Utility } from "../helpers/utility";
 import { AutomatonDefinition, Color, AutomatonFunctions } from "../library/types";
 
 export namespace Fire {
+    const TREE_LIKELIHOOD = .01 as const;
+    const GRASS_LIKELIHOOD = .25 as const;
+
     export enum States {
         Tree = "#003300",
         Grass = "#006600",
@@ -15,29 +18,20 @@ export namespace Fire {
     export const Update = (current: States, neighbors: Neighborhood<States>) => {
         switch (current) {
             case States.Tree:
-                return catchesFire(.1, neighbors) ? States.TreeFire : States.Tree;
+                return catchesFire(TREE_LIKELIHOOD, neighbors) ? States.TreeFire : States.Tree;
             case States.Grass:
-                return catchesFire(.15, neighbors) ? States.GrassFire : States.Grass;
+                return catchesFire(GRASS_LIKELIHOOD, neighbors) ? States.GrassFire : States.Grass;
 
             case States.GrassFire:
-                return neighbors.GetMooreCount(
-                    (v) => Utility.isAnyOf(v,
-                        States.TreeFire, States.GrassFire,
-                        States.BurnedGrass, States.BurnedTree)) === 8
+                return neighbors.GetMooreCount((v) => isBurningOrBurned(v)) === 8
                     ? States.BurnedGrass : States.GrassFire;
 
             case States.TreeFire:
-                return neighbors.GetMooreCount(
-                    (v) => Utility.isAnyOf(v,
-                        States.TreeFire, States.GrassFire,
-                        States.BurnedGrass, States.BurnedTree)) === 8
+                return neighbors.GetMooreCount((v) => isBurningOrBurned(v)) === 8
                     ? States.BurnedTree : States.TreeFire;
 
-            case States.BurnedGrass:
-                return States.BurnedGrass;
-            case States.BurnedTree:
-                return States.BurnedTree;
-
+            case States.BurnedGrass: return States.BurnedGrass;
+            case States.BurnedTree: return States.BurnedTree;
             default:
                 throw new Error("Invalid state in Water");
         }
@@ -80,6 +74,13 @@ export namespace Fire {
     function catchesFire(likelihood: number, neighbors: Neighborhood<States>) {
         const count = neighbors.GetMooreCount(
             (v) => Utility.isAnyOf(v, States.TreeFire, States.GrassFire));
-        return ((count / 10) * likelihood) > Math.random();
+        return ((count / 8) * likelihood) > Math.random();
+    }
+
+    function isBurningOrBurned(state: States) {
+        return Utility.isAnyOf(state,
+            States.TreeFire, States.GrassFire,
+            States.BurnedGrass, States.BurnedTree
+        );
     }
 }
